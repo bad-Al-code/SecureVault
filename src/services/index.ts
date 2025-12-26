77201adb8c5762167a935c0621f00473fdb0d9b7
Badal <badalsingh120403@gmail.com>
2025-12-26 17:26:09 +0530

    feat: add S3Service for AWS integration

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,6 @@ export * from './config.service';
 export * from './crypto.service';
 export * from './editor.service';
 export * from './file.service';
+export * from './s3.service';
 export * from './vault-action.service';
 export * from './version-control.service';
